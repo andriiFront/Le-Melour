a0252168ea9f19862708069afc3fa4e87568db5e
Andrii <badekha@gmail.com>
2021-11-18 20:30:13 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import { Footer } from './components/Footer';
 // import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <div className="App" />
   );
 }
 
